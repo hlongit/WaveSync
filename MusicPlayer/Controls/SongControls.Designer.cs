80b986d657c4986d25d6a9313b85f14e9d230480
TranVinh23521797 <23521797@gm.uit.edu.vn>
2025-12-06 03:42:48 +0700

    Update SongControls.Designer.cs

--- MusicPlayer/Controls/SongControls.Designer.cs
+++ MusicPlayer/Controls/SongControls.Designer.cs
@@ -1,32 +1,71 @@
-﻿namespace MusicPlayer.Controls {
-    partial class SongControls {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
+namespace MusicPlayer.Controls
+{
+    partial class SongControls
+    {
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Component Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent() {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblArtist = new System.Windows.Forms.Label();
+            this.lblAlbum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(10, 5);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(260, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Song Title";
+            // 
+            // lblArtist
+            // 
+            this.lblArtist.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblArtist.Location = new System.Drawing.Point(10, 32);
+            this.lblArtist.Name = "lblArtist";
+            this.lblArtist.Size = new System.Drawing.Size(200, 20);
+            this.lblArtist.TabIndex = 1;
+            this.lblArtist.Text = "Artist";
+            // 
+            // lblAlbum
+            // 
+            this.lblAlbum.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblAlbum.Location = new System.Drawing.Point(220, 22);
+            this.lblAlbum.Name = "lblAlbum";
+            this.lblAlbum.Size = new System.Drawing.Size(110, 20);
+            this.lblAlbum.TabIndex = 2;
+            this.lblAlbum.Text = "Album";
+            this.lblAlbum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // SongItem
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.lblAlbum);
+            this.Controls.Add(this.lblArtist);
+            this.Controls.Add(this.lblTitle);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "SongItem";
+            this.Size = new System.Drawing.Size(340, 60);
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblArtist;
+        private System.Windows.Forms.Label lblAlbum;
     }
 }

66495f9165aa74b76d3eb15459f31f1e895514e2
TranVinh23521797 <23521797@gm.uit.edu.vn>
2025-12-12 14:33:48 +0700

    Add files via upload

---
+++ MusicPlayer/Forms/UserSettingForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MusicPlayer.Forms
+{
+    partial class UserSettingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AvatarBox = new System.Windows.Forms.PictureBox();
+            this.btnChangeAvatar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAvatarPath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AvatarBox
+            // 
+            this.AvatarBox.Location = new System.Drawing.Point(109, 67);
+            this.AvatarBox.Name = "AvatarBox";
+            this.AvatarBox.Size = new System.Drawing.Size(100, 95);
+            this.AvatarBox.TabIndex = 0;
+            this.AvatarBox.TabStop = false;
+            // 
+            // btnChangeAvatar
+            // 
+            this.btnChangeAvatar.Location = new System.Drawing.Point(109, 168);
+            this.btnChangeAvatar.Name = "btnChangeAvatar";
+            this.btnChangeAvatar.Size = new System.Drawing.Size(100, 46);
+            this.btnChangeAvatar.TabIndex = 1;
+            this.btnChangeAvatar.Text = "Change Avatar";
+            this.btnChangeAvatar.UseVisualStyleBackColor = true;
+            this.btnChangeAvatar.Click += new System.EventHandler(this.btnChangeAvatar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(304, 64);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(206, 22);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(362, 120);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 45);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Avatar Path";
+            // 
+            // txtAvatarPath
+            // 
+            this.txtAvatarPath.Location = new System.Drawing.Point(304, 92);
+            this.txtAvatarPath.Name = "txtAvatarPath";
+            this.txtAvatarPath.Size = new System.Drawing.Size(206, 22);
+            this.txtAvatarPath.TabIndex = 7;
+            // 
+            // UserSettingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAvatarPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnChangeAvatar);
+            this.Controls.Add(this.AvatarBox);
+            this.Name = "UserSettingForm";
+            this.Text = "UserSettingForm";
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AvatarBox;
+        private System.Windows.Forms.Button btnChangeAvatar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAvatarPath;
+    }
+}
---
+++ MusicPlayer/Forms/UserSettingForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MusicPlayer.Core;
+
+namespace MusicPlayer.Forms
+{
+    public partial class UserSettingForm : Form
+    {
+        public UserSettingForm()
+        {
+            InitializeComponent();
+
+            string avatarFile;
+            try
+            {
+                avatarFile = DatabaseHelper.GetActiveAvatar(CurrentUser.UserID);
+            }
+            catch
+            {
+                avatarFile = null;
+            }
+
+            // Load into AvatarBox using helper that avoids file-locking and handles paths
+            LoadAvatarIntoBox(avatarFile);
+        }
+
+        private void LoadAvatarIntoBox(string avatarFile)
+        {
+            string avatarPath;
+            if (string.IsNullOrWhiteSpace(avatarFile))
+            {
+                avatarPath = Path.Combine(Application.StartupPath, "Avatars", "default.png");
+            }
+            else if (Path.IsPathRooted(avatarFile))
+            {
+                avatarPath = avatarFile;
+            }
+            else
+            {
+                // Some DB entries store only the filename (e.g. "user1.png")
+                // or a relative path like "Avatars\\user1.png" — handle both.
+                if (avatarFile.IndexOf("Avatars", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    avatarPath = Path.Combine(Application.StartupPath, avatarFile);
+                }
+                else
+                {
+                    avatarPath = Path.Combine(Application.StartupPath, "Avatars", avatarFile);
+                }
+            }
+
+            // If file is missing, use default image (avoid throwing)
+            if (File.Exists(avatarPath))
+            {
+                AvatarBox.Image = Image.FromFile(avatarPath);
+            }
+            else
+            {
+                // Use a bundled default or leave null
+                string defaultPath = Path.Combine(Application.StartupPath, "Avatars", "default.png");
+                AvatarBox.Image = File.Exists(defaultPath) ? Image.FromFile(defaultPath) : null;
+            }
+        }
+
+        private void btnChangeAvatar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Images|*.png;*.jpg;*.jpeg";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string newFile = Path.Combine("Avatars", CurrentUser.Username + ".png");
+                File.Copy(dlg.FileName, newFile, true);
+
+                // cập nhật database
+                CurrentUser.AvatarPath = newFile;
+                DatabaseHelper.UpdateAvatar(CurrentUser.UserID, newFile);
+                MessageBox.Show("Avatar updated!");
+                this.Close();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            CurrentUser.Username = txtUsername.Text;
+        }
+    }
+}
